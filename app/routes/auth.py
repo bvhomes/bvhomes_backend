9b87b71f7f0f2c610d85f255d64feac72e655ada
MadhuArigela <madhusimhadri955@gmail.com>
2026-02-24 02:24:40 +0530

    Updated AUTH

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -5,6 +5,7 @@ from app.models.user import User
 from app.schemas.user import UserCreate, UserResponse
 from app.core.security import hash_password, verify_password
 from app.core.auth import create_access_token
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
@@ -33,10 +34,13 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
     return new_user
 
 @router.post("/login")
-def login(email: str, password: str, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.email == email).first()
+def login(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db)
+):
+    user = db.query(User).filter(User.email == form_data.username).first()
 
-    if not user or not verify_password(password, user.hashed_password):
+    if not user or not verify_password(form_data.password, user.hashed_password):
         raise HTTPException(status_code=401, detail="Invalid credentials")
 
     access_token = create_access_token({"sub": user.email})
